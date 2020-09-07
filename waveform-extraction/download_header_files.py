f79b2f33a912d3126c724d639b9467d2f787f179
hamidk997 <44320326+hamidk997@users.noreply.github.com>
2020-09-07 04:44:04 +0100

    Update download_header_files.py

--- waveform-extraction/download_header_files.py
+++ waveform-extraction/download_header_files.py
@@ -4,28 +4,19 @@ from tqdm import tqdm
 import argparse
 
 path_to_patient_ids = 'https://archive.physionet.org/physiobank/database/mimic3wdb/matched/RECORDS'
-save_dir = "/Volumes/Elements/all_header_files/"
 
 f = urlopen(path_to_patient_ids)
 patient_paths = f.read().decode('utf-8').split('\n')
 
 parser = argparse.ArgumentParser()
-parser.add_argument("lim1", nargs='?', default=0)
-parser.add_argument("lim2", nargs='?', default=len(patient_paths))
+parser.add_argument("save_dir", nargs='?', default=0)
 args = parser.parse_args()
 
-print(args.lim1, args.lim2)
 print('DOWNLOADING HEADER FILES')
 
-i = 0
 
-for patient in tqdm(patient_paths[678:680]):
-    if i == 0:
-        print(patient_paths[int(args.lim1)])
-        print(patient_paths[int(args.lim2) - 1])
-        i = i + 1
-
-    save_path = os.path.join(save_dir,patient)
+for patient in tqdm(patient_paths):
+    save_path = os.path.join(args.save_dir,patient)
 
     os.makedirs((save_path), exist_ok=True)
     command = f"rsync -CaLvz -q --delete  --include='*.hea'    --include='*/' --exclude='*' --prune-empty-dirs  " \
